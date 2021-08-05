e4120a152451533aa4be0d6d5f2d4ba09c1bf0aa
Philip Mateescu <github@philipmat.net>
2021-08-04 20:33:11 -0500

    don't need hosting either

--- EF Conventions/Program.cs
+++ EF Conventions/Program.cs
@@ -2,10 +2,6 @@
 using System.Linq;
 using EF_Conventions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
-
-// using IHost host = Host.CreateDefaultBuilder(args).Build();
-// await host.RunAsync();
 
 using var ctx = new SampleContext();
 
